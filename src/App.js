4083df52bd38ccece710861ad70d380dc299f1ab
Enso <aarmand.inbox@gmail.com>
2018-09-23 10:24:48 +0200

    Add Apollo Server and Client'

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+const client = new ApolloClient({
+  uri: "/.netlify/functions/graphql"
+});
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
-  }
+// Replace the previous LambdaDemo with the code below:
+const LambdaDemo = () => (
+  <ApolloProvider client={client}>
+    <Query
+      query={gql`
+        {
+          hello
+        }
+      `}
+    >
+      {({ data }) =>
+        <div>A greeting from the server: {data.hello}</div>}
+    </Query>
+  </ApolloProvider>
+);
+// class LambdaDemo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {loading: false, msg: null};
+//   }
 
-  handleClick = (e) => {
-    e.preventDefault();
+//   handleClick = (e) => {
+//     e.preventDefault();
 
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-  }
+//     this.setState({loading: true});
+//     fetch('/.netlify/functions/hello')
+//       .then(response => response.json())
+//       .then(json => this.setState({loading: false, msg: json.msg}));
+//   }
 
-  render() {
-    const {loading, msg} = this.state;
+//   render() {
+//     const {loading, msg} = this.state;
 
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
-  }
-}
+//     return <p>
+//       <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
+//       <span>{msg}</span>
+//     </p>
+//   }
+// }
 
 class App extends Component {
   render() {
